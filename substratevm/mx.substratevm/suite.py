54540515f18ae0e3e441f7df3d9c1c3603b3f522
Ziyi Lin <cengfeng.lzy@alibaba-inc.com>
2025-03-05 18:57:08 +0800

    Use class-file API instead of ASM

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -1180,7 +1180,6 @@ suite = {
                     "jdk.internal.misc",
                     "jdk.internal.vm",
                     "sun.security.jca",
-                    "jdk.internal.org.objectweb.asm"
                 ],
                 "jdk.internal.vm.ci": [
                     "jdk.vm.ci.meta"
